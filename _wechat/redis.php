9860f4da2c532377a33b4889ce4945d2e4fb9aca
vc <52953973@qq.com>
2020-11-09 15:08:29 +0800

    fixed

---
+++ _wechat/redis.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/4/14
+ * Time: 18:17
+ */
+return [
+    'host'      => '192.168.3.199',
+    'port'      => 6379,
+    'database' => 4,
+    'timeout'    => 4,
+//    'persistent' => false,
+//    'prefix'     => '',
+//    'serialize'  => true,
+];
